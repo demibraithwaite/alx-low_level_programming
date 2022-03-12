71ce3b9d7bfc61cd13e62a8e92a572db9548e19d
fhoyinn <whoisfhoyinn@gmail.com>
2022-03-12 08:46:29 -0800

    C-var.6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - print simgle digit numbers using putchar
+ *
+ * Return: 0, Everytime.
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+
+
+}
+
+
