f8b8d0726b78246f54cc12a3b55f83b2e2438dfa
fhoyinn <whoisfhoyinn@gmail.com>
2022-03-12 08:35:47 -0800

    C-var.5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print all the single digits numbers
+ *
+ * Return: 0, Everytime.
+ *
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+
+	printf("\n");
+
+	return (0);
+
+
+}
