ee89fb0b04242c082b878990cc46c7ae1bd3a38d
fhoyinn <whoisfhoyinn@gmail.com>
2022-03-12 09:07:26 -0800

    C-var.7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabets in reverse
+ *
+ * Return: 0, Everytime.
+ *
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
