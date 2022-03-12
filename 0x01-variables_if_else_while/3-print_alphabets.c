1bdece905577a6e724088e1828ddd87524217143
fhoyinn <whoisfhoyinn@gmail.com>
2022-03-11 16:45:23 -0800

    C-var.3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet using putchar
+ *
+ * Return: 0, Everytime.
+ *
+ */
+
+int main(void)
+{
+	char c;
+	char d;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (d = 'A'; d <= 'Z'; d++)
+	{
+		putchar(d);
+	}
+	putchar('\n');
+
+	return (0);
+
+}
