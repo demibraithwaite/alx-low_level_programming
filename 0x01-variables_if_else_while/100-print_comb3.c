2e86594fe4f9d054138e3219415b2ca2bdb9771e
fhoyinn <whoisfhoyinn@gmail.com>
2022-03-14 12:56:09 -0700

    C-var.100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - print different combinations of two digits
+ *
+ * Return: 0, Everytime.
+ *
+ */
+
+int main(void)
+{
+	int n;
+	int m;
+
+	for (n = 48; n <= 57; n++)
+	{
+		for (m = n + 1; m <= 57; m++)
+		{
+			putchar(n);
+			putchar(m);
+			if (n != 56)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+
+	return (0);
+
+}
