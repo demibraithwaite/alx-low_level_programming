94464008a4908e7828e79d167475484bf31997e5
fhoyinn <whoisfhoyinn@gmail.com>
2022-04-05 12:56:30 -0700

    static lib and tings

---
+++ 0x09-static_libraries/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Return: 0, Everytime.
+ *
+ */
+
+int main(void)
+{
+	printf("%s", "_putchar");
+	printf("\n");
+	return (0);
+}
+
+
---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * _putchar.c - writes characters
+ *
+ * Returns: Always, 0.
+ */
+
+int _putchar(char c)
+{
+
+}
