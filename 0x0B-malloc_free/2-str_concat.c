11d2b7435ef01bf1cf5ed9089cf63346974f18bf
fhoyinn <whoisfhoyinn@gmail.com>
2022-04-06 18:41:57 -0700

    malloc and free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,52 +1,27 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 /**
- * _strlen - Swaps integers with pointers.
+ * str_concat - a fxn that concatenates two strings
+ * @s1: string 1
+ * @s2: string 2
  *
- * @s: is a pointer to a char
- *
- * Return: Always 0.
+ * Return: pointer to concatenated string
  */
-int _strlen(char *s)
-{
-int i = 0;
-while (*(s + 1) != '\0')
-i++;
-return (i);
-}
-/**
- * str_concat - Concatenate 2 strings
- *
- * @s1: First string.
- * @s2: Second string.
- *
- * Return: Returns the created array.
- */
+
 char *str_concat(char *s1, char *s2)
 {
-int i, j, size1, size2, totSize;
-char *ar;
-if (s1 == NULL)
-s1 = "";
-if (s2 == NULL)
-s2 = "";
-size1 = _strlen(s1);
-size2 = _strlen(s2);
-totSize = (size1 + size2)+1;
-ar = malloc(totSize *sizeof(char));
-if (ar == NULL)
-return (NULL);
-else
-{
-for (i = 0; i < size1; i++)
-ar[i] = s1[i];
-for (j = 0; j < size2; j++)
-{
-ar[i] = s2[j];
-i++;
-}
-ar[i + 1] = '\0';
-return (ar);
-}
+	int i, l1, l2;
+	char *str;
+
+	l1 = s1 == NULL ? 0 : strlen(s1);
+	l2 = s2 == NULL ? 0 : strlen(s2);
+	str = malloc(l1 + l2 + 1);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; i < l1; i++)
+		str[i] = s1[i];
+	for (i = 0; i < l2; i++)
+		str[l1 + i] = s2[i];
+	return (str);
 }
